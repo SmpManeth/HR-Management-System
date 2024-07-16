459ae7956145009855679afd180b922bddaa2983
SmpManeth <smpmaneth@gmail.com>
2024-07-16 15:20:46 +0530

    empl id by asscending

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -14,7 +14,7 @@ class EmployeeController extends Controller
     public function index()
     {
         return view('pages.employees.index')->with([
-            'employees' => Employee::all(),
+            'employees' => Employee::orderBy('Employee_ID', 'asc')->get(),
         ]);
     }
 
